0ef887d099c3bdb5c73a9b575a253e04a9ffbd8c
Mariangeles Mavarez <mariangelesmavarez@gmail.com>
2025-10-30 16:24:25 -0400

    feat: completed Suma section

--- Proyecto1/script.js
+++ Proyecto1/script.js
@@ -6,6 +6,14 @@ function alertDimensionesDistintas(){
     window.alert("Ambas matrices deben tener las mismas dimensiones");
 }
 
+function alertResultado(){
+    window.alert("Por favor ingrese todos valores para todas las celdas");
+}
+
+function alertaVaciar(){
+    window.alert("Las celdas deben estar vacias");
+}
+
 function createMatriz(c, r, nombreMatriz){
     //crea Grid
     let matrixGrid = document.createElement('div');
@@ -31,47 +39,39 @@ function createMatriz(c, r, nombreMatriz){
     }
     return matrixGrid;
 }
-/*
-btCrearSuma.addEventListener("click", () => {
-    let sizeMatrizA = document.getElementById("sizeMatrizA").value;
-    let sizeMatrizB = document.getElementById("sizeMatrizB").value;
 
-    if(!Boolean(sizeMatrizA) ||  !Boolean(sizeMatrizB)){
-        alertDimensionesIncompletas();
-        return;
+function matrizCompleta(idMatriz) {
+    const matriz = document.getElementById(idMatriz);
+
+    // Selecciona todos los inputs dentro del contenedor
+    const celdas = matriz.querySelectorAll("input[type='number']");
+
+    //una por una
+    for (let celda of celdas) {
+        if (celda.value.trim() == "") {
+            return false; // hay al menos una vacía
+        }
     }
 
-    //ambas dimensiones estan
-    let a=parseInt(sizeMatrizA);
-    let b=parseInt(sizeMatrizB);
-    if(a!=b){
-        alertDimensionesDistintas();
-        return;
+    return true; // todas las celdas tienen valor
+}
+
+function matrizEmpty(idMatriz) {
+    const matriz = document.getElementById(idMatriz);
+
+    // Selecciona todos los inputs dentro del contenedor
+    const celdas = matriz.querySelectorAll("input[type='number']");
+
+    //una por una
+    for (let celda of celdas) {
+        if (celda.value.trim() != "") {
+            return false; // hay al menos una celda con algo
+        }
     }
 
-    // Ajusta la altura de la sección
-    let seccionSuma = document.getElementById("suma");
-    seccionSuma.style.height = "auto";
+    return true; // todas las celdas esta vacias
+}
 
-    const contenidoSuma= document.getElementById("contenidoSuma");
-    contenidoSuma.hidden=false;
-    seccionSuma.style.borderRadius= "20px";
-    let matrixA =createMatriz(a, a, 'matriz-suma-A');
-    let matrixB=createMatriz(b, b, 'matriz-suma-B');
-    //containers
-    let seccionMatrizA=document.getElementById("seccionSumaMatrizA");
-    let seccionMatrizB=document.getElementById("seccionSumaMatrizB")
-    //plus
-    let plusSymbol=document.createElement('p');
-    plusSymbol.textContent="+";
-    //equal
-    let equalSymbol=document.createElement('p');
-    equalSymbol.textContent="=";
-    seccionMatrizA.appendChild(matrixA);
-    seccionMatrizB.appendChild(matrixB);
-
-})
-*/
 const handleCrearSuma=()=>{
     let sizeMatrizA = document.getElementById("sizeMatrizA").value;
     let sizeMatrizB = document.getElementById("sizeMatrizB").value;
@@ -114,24 +114,106 @@ const handleCrearSuma=()=>{
         return;
     }
     else{
-        let seccionMatrizA=document.getElementById("seccionSumaMatrizA");
-        let seccionMatrizB=document.getElementById("seccionSumaMatrizB");
-
-        let matrixA=document.getElementById('matriz-suma-A');
-        let matrixB=document.getElementById('matriz-suma-B');
-
-        seccionMatrizA.removeChild(matrixA);
-        seccionMatrizB.removeChild(matrixB);
-
-        contenidoSuma.hidden=true;
-        handleCrearSuma();
+        clearSum();
         return;
     }
 
 };
 
 
+const handleResultadoSuma=()=>{
+    const contenidoSuma= document.getElementById("contenidoSuma");
+    
+    if(!matrizCompleta('matriz-suma-A') || !matrizCompleta('matriz-suma-B')){
+        alertResultado();
+        return;
+    }
+    //si estan las matrices completas, crea la otra matriz y resuelve
+
+    let matrixA=document.getElementById('matriz-suma-A');
+    let matrixB=document.getElementById('matriz-suma-B');
+
+    //size
+    let sizeMatrizResultado= parseInt(document.getElementById("sizeMatrizA").value);
+    const matrixResultado= createMatriz(sizeMatrizResultado, sizeMatrizResultado, 'matriz-suma-resultado');
+    //adjunta
+    const seccionSumaResultado = document.getElementById("seccionSumaResultado");
+    seccionSumaResultado.appendChild(matrixResultado);
+    seccionSumaResultado.hidden=false;
+    //llena
+    for(let i = 1; i <=sizeMatrizResultado; i++){
+        for(let j = 1; j <=sizeMatrizResultado; j++){
+            let matrixCellR= document.getElementById(`matriz-suma-resultado_${i}_${j}`); 
+            let matrixCellA= document.getElementById(`matriz-suma-A_${i}_${j}`);
+            let matrixCellB= document.getElementById(`matriz-suma-B_${i}_${j}`);
+            matrixCellR.value= Number(matrixCellA.value)+Number(matrixCellB.value);
+        }
+    }
+};
+
+const clearSum = ()=>{
+    //secciones
+    const seccionSumaResultado = document.getElementById("seccionSumaResultado");
+    const seccionMatrizA=document.getElementById("seccionSumaMatrizA");
+    const seccionMatrizB=document.getElementById("seccionSumaMatrizB");
+    //matrices
+    const matrixA=document.getElementById('matriz-suma-A');
+    const matrixB=document.getElementById('matriz-suma-B');
+    const matrixResultado=document.getElementById('matriz-suma-resultado');
+
+    //remove
+    if (seccionMatrizA.contains(matrixA)) {seccionMatrizA.removeChild(matrixA)};
+    if (seccionMatrizB.contains(matrixB)) {seccionMatrizB.removeChild(matrixB)};
+    if (seccionSumaResultado.contains(matrixResultado)) {seccionSumaResultado.removeChild(matrixResultado)};
+    //ocultar
+    const contenidoSuma= document.getElementById("contenidoSuma");
+    seccionSumaResultado.hidden=true;
+    contenidoSuma.hidden=true;
+
+};
+
+function randomNumberForCell(){ //IN RANGE[-10, 10]
+    return Number((Math.random()*(20)-10).toFixed(1));
+}
+
+const handleGenerarValores=(c, r, matrixID)=>{
+
+    const matrixToFill= document.getElementById(matrixID);
+    
+    //si esta la matriz vacia
+
+    for(let i = 1; i <=c; i++){
+        for(let j = 1; j <=r; j++){
+            let matrixCell= document.getElementById(`${matrixID}_${i}_${j}`);
+
+            matrixCell.value= randomNumberForCell();  
+        }
+    }
+};
+
 
 const btCrearSuma=document.getElementById("crearSuma");
 btCrearSuma.addEventListener("click", handleCrearSuma);
-    
+
+const btResultado=document.getElementById("btResultadoSuma");
+btResultado.addEventListener("click", handleResultadoSuma);
+
+const  btLimpiarSuma= document.getElementById("btLimpiarSuma");
+btLimpiarSuma.addEventListener("click", clearSum);
+
+const  btGenerarSuma= document.getElementById("btGenerarValores");
+btGenerarSuma.addEventListener("click", ()=>{
+
+    if(!matrizEmpty('matriz-suma-A') || !matrizEmpty('matriz-suma-B')  ){
+        alertaVaciar();
+        return;
+    }
+    let sizeMatrizA= parseInt(document.getElementById("sizeMatrizA").value);
+    let sizeMatrizB= parseInt(document.getElementById("sizeMatrizB").value);
+
+    handleGenerarValores(sizeMatrizA, sizeMatrizA, 'matriz-suma-A');
+    handleGenerarValores(sizeMatrizB, sizeMatrizB, 'matriz-suma-B');
+
+
+} )
+
